d771f34949a3d09fd99df5b0b68c6ce3da89264f
caosjr <carlos_bertojr@hotmail.com>
2019-06-03 13:59:07 -0300

    Update main.vhd

--- main.vhd
+++ main.vhd
@@ -3,27 +3,26 @@ use IEEE.std_logic_1164.all;
 use ieee.numeric_std.ALL;  
 
 entity main is
-	port(	clk		: in std_logic;
-    		reset		: in std_logic;
-         result	: out std_logic_vector(3 downto 0);
-			display	: out std_logic_vector(6 downto 0)
-    	);
+	port(clk		: in std_logic;
+	     reset		: in std_logic;
+	     result	: out std_logic_vector(3 downto 0);
+	     display	: out std_logic_vector(6 downto 0)
+	    );
 end main;
 
 architecture arq of main is
-
 	component sleep_sort is
 		port (clk		: in std_logic;
-				reset		: in std_logic;
-				data		: in std_logic_vector (3 downto 0);
-				ready		: out std_logic
-				);
+		      reset		: in std_logic;
+		      data		: in std_logic_vector (3 downto 0);
+		      ready		: out std_logic
+		     );
 	end component;
 	
 	component display7seg is
 		port (data	:	in std_logic_vector(3 downto 0);
-				output:	out std_logic_vector(6 downto 0)
-			  );
+		      output:	out std_logic_vector(6 downto 0)
+		     );
 	end component;
 
 	subtype word is STD_LOGIC_VECTOR (3 downto 0);
@@ -49,28 +48,29 @@ architecture arq of main is
 		return tmp;
 	end result_table;
 	
-	signal table	 			: memory_table := init_table;
+	signal table			: memory_table := init_table;
 	signal output_table		: memory_table := result_table;
-	signal data					: std_logic_vector (3 downto 0) := (others => '0');
-	signal bus_ready			: std_logic_vector (3 downto 0) := (others => '0');
+	signal data			: std_logic_vector (3 downto 0) := (others => '0');
+	signal bus_ready		: std_logic_vector (3 downto 0) := (others => '0');
 	
 begin
 	g_sp : for I in 0 to 3 generate
 		sp : sleep_sort port map(clk => clk, reset => reset, data => table(I), ready => bus_ready(I));
 	end generate g_sp;
+			
 	d1 : display7seg 	port map(data => data, output => display);
 	
 	read_list : process(clk)
-		variable t 			: integer range 0 to 4 := 0;
+		variable t 		: integer range 0 to 4 := 0;
 		variable counter 	: integer range 0 to 3 := 0;
-		variable delay 	: std_logic_vector(31 downto 0) := (others => '0');
+		variable delay		: std_logic_vector(31 downto 0) := (others => '0');
 	begin
-    	if (rising_edge(clk)) then
+		if (rising_edge(clk)) then
 			if (t /= 4) then
 				ffs: for J in 3 downto 0 loop
 					if (bus_ready(J) = '1') then
 						output_table(t)	<= table(J);
-						t 						:= t + 1;
+						t 		:= t + 1;
 					end if;
 				end loop;
 			else
@@ -78,11 +78,11 @@ begin
 					delay 	:= std_logic_vector(unsigned(delay) + 1);
 				else
 					result 	<= output_table(counter);
-					data		<= output_table(counter);
+					data	<= output_table(counter);
 					counter	:= counter + 1;
-					delay		:= (others => '0');
+					delay	:= (others => '0');
 				end if;
 			end if;
-      end if;
-   end process read_list;
+		end if;
+   	end process read_list;
 end arq;
