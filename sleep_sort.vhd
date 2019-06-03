df6e875e5a5568fd79b2cc37513bdec7066bb6df
caosjr <carlos_bertojr@hotmail.com>
2019-06-03 14:29:37 -0300

    Update sleep_sort.vhd

--- sleep_sort.vhd
+++ sleep_sort.vhd
@@ -3,34 +3,33 @@ use IEEE.std_logic_1164.all;
 use ieee.numeric_std.ALL;  
 
 entity sleep_sort is
-	port(	clk		: in std_logic;
-    		reset		: in std_logic;
-    		data		: in std_logic_vector (3 downto 0);
-			ready		: out std_logic := '0'
-    	);
+	port(clk		: in std_logic;
+	     reset		: in std_logic;
+	     data		: in std_logic_vector (3 downto 0);
+	     ready		: out std_logic := '0'
+	    );
 end sleep_sort;
 
 architecture algorithm of sleep_sort is
 begin
-	sleep : process(clk)
-    variable clock_pulse : std_logic_vector(3 downto 0) := (others => '0');
-	 variable used			 : std_logic := '0';
-	 --variable seconds : integer range 0 to 2;--50000000; -- 1 segundo para um clk de 50mhz
+    sleep : process(clk)
+	variable clock_pulse : std_logic_vector(3 downto 0) := (others => '0');
+	variable used			 : std_logic := '0';
     begin
     	if (rising_edge(clk)) then
         	if (reset = '1') then
-				ready 		<= '0';
-				clock_pulse := (others => '0');
-			elsif (clock_pulse < data) then
-				clock_pulse 	:= std_logic_vector(unsigned(clock_pulse) + 1);
+			ready 		<= '0';
+			clock_pulse := (others => '0');
+		elsif (clock_pulse < data) then
+			clock_pulse 	:= std_logic_vector(unsigned(clock_pulse) + 1);
+		else
+			if (used = '0') then
+				ready <= '1';
+				used 	:= '1';
 			else
-				if (used = '0') then
-					ready <= '1';
-					used 	:= '1';
-				else
-					ready <= '0';
-				end if;
-         end if;
-      end if;
+				ready <= '0';
+			end if;
+		end if;
+	end if;
    end process sleep;
 end algorithm;
